47d598e890b6c1d495c75483697eb12952b2bfb4
smahmoodh <seyedmahmoodhosseini@gmail.com>
2024-12-09 21:42:34 +0330

    some fix

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,30 +1,25 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const Form = ({type, post, setPost, submitting, setSubmitting, handleSubmit}) => {
+const Form = ({ type, post, setPost, submitting, setSubmitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} Post</span>
       </h1>
 
-      <p className="desc text-left max-w-md">
-        {type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.
-      </p>
+      <p className="desc text-left max-w-md">{type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered platform.</p>
 
-      <form
-        onSubmit={handleSubmit}
-        className="mt-8 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
-      >
+      <form onSubmit={handleSubmit} className="mt-8 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
         <label>
-          <span className="font-satoshi font-semibold text-base text-gray-700">
-            Your AI Prompt
-          </span>
+          <span className="font-satoshi font-semibold text-base text-gray-700">Your AI Prompt</span>
           <textarea
             value={post.prompt}
-            onChange={(e) => setPost({
-              ...post,
-              prompt: e.target.value
-            })}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                prompt: e.target.value,
+              })
+            }
             placeholder="write your prompt here..."
             required
             className="form_textarea"
@@ -32,15 +27,16 @@ const Form = ({type, post, setPost, submitting, setSubmitting, handleSubmit}) =>
         </label>
         <label>
           <span className="font-satoshi font-semibold text-base text-gray-700">
-            Tag {' '}
-            <span className="font-normal">(#product, #webdevelopment, #idea)</span>
+            Tag <span className="font-normal">(#product, #webdevelopment, #idea)</span>
           </span>
           <input
             value={post.tag}
-            onChange={(e) => setPost({
-              ...post,
-              tag: e.target.value
-            })}
+            onChange={(e) =>
+              setPost({
+                ...post,
+                tag: e.target.value,
+              })
+            }
             placeholder="#tag"
             required
             className="form_input"
@@ -48,20 +44,16 @@ const Form = ({type, post, setPost, submitting, setSubmitting, handleSubmit}) =>
         </label>
 
         <div className="flex-end mx-3 mb-5 gap-5">
-          <Link href='/' className="text-gray-500 text-sm">
+          <Link href="/" className="text-gray-500 text-sm">
             Cancel
           </Link>
-          <button
-            type="submit"
-            disabled={submitting}
-            className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white"
-          >
+          <button type="submit" disabled={submitting} className="px-5 py-1.5 text-sm bg-primary-orange rounded-full text-white">
             {submitting ? `${type}...` : type}
           </button>
         </div>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
